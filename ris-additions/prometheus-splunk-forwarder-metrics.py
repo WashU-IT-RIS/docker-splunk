9a4811a0fa6ab54ee3cfec7133f529c0925cf46c
Anthony Brummett <brummett@gmail.com>
2021-10-27 14:04:22 -0500

    Make the Splunk query to get a count of this host's messages
    in the last hour
    
    ITDEV-29896

--- ris-additions/prometheus-splunk-forwarder-metrics.py
+++ ris-additions/prometheus-splunk-forwarder-metrics.py
@@ -2,11 +2,19 @@
 
 import re
 import sys
+import os
 import time
 import traceback
+import socket
+
+sys.path.insert(0, os.path.join( os.path.dirname(__file__), '/splunklibs/lib/python2.7/site-packages'))
+from splunklib.client import connect as splunk_connect
+import splunklib.results as splunk_results_parser
 
 splunk_config_file = '/opt/splunkforwarder/etc/apps/SplunkUniversalForwarder/local/inputs.conf'
 loop_sleep_time_seconds = 60
+expected_net_dev_name = 'eth0'
+splunk_search_hostname = 'machinedata.wustl.edu'
 
 def resolve_host_from_splunk_config(config_file):
     find_host_config = re.compile(r'host\s*=\s*(\S+)')
@@ -18,7 +26,7 @@ def resolve_host_from_splunk_config(config_file):
     raise LookupError('Cannot find "host" config key in %s' % ( config_file ))
 
 # Assumes the container has a 'lo' and one other net device
-def resolve_net_device():
+def resolve_net_device(expected_dev_name):
     netdev_find = re.compile(r'\s*(\w+):')
     devices_found = { }
     with open('/proc/net/dev') as fh:
@@ -31,14 +39,10 @@ def resolve_net_device():
                 dev_name = match.group(1)
                 devices_found[dev_name] = True
 
-    # Don't care about the loopback device
-    devices_found.pop('lo', None)
-
-    device_names = devices_found.keys()
-    if len(device_names) > 1:
+    if expected_dev_name in devices_found:
+        return expected_dev_name
+    else:
         raise LookupError('Found more than one non-loopback network device: %s' % ( device_names ))
-
-    return device_names[0]
 
 def collect_stats_for_netdev(name):
     find_name = re.compile(r'\s*' + name + r':')
@@ -53,17 +57,40 @@ def collect_stats_for_netdev(name):
                     raise ValueError('Could not extract tx/tx packets and bytes from /proc/net/dev for device ' + name)
     raise LookupError('Could not find device named ' + name + ' in /proc/net/dev')
 
-def collect_message_stats_from_splunk(host):
-    pass
+def collect_message_stats_from_splunk(my_hostname, splunk_server, username, password):
+    service = splunk_connect(username=username, password=password, host=splunk_server)
+    socket.setdefaulttimeout(None)
+    query = 'search index=prod_ris_syslog earliest_time=-1h host=' + my_hostname + ' | stats count'
+    response = service.jobs.oneshot(query)
+
+    for result in splunk_results_parser.ResultsReader(response):
+        # Should only be one row of results
+        return { 'messages_last_hour': result['count'] }
+
+def record_stats(net_stats, msg_stats):
+    all_stats = {
+        'splunk_container_tx_bytes':   net_stats['tx_bytes'],
+        'splunk_container_tx_packets': net_stats['tx_packets'],
+        'splunk_container_rx_bytes':   net_stats['rx_bytes'],
+        'splunk_container_rc_packets': net_stats['rx_packets'],
+        'splunk_container_host_messages_last_hour': msg_stats['messages_last_hour'],
+        'splunk_container_timstamp':   time.time(),
+    }
+    print all_stats
 
 def main():
     host = resolve_host_from_splunk_config(splunk_config_file)
-    netdev = resolve_net_device()
+    netdev = resolve_net_device(expected_net_dev_name)
 
     while True:
         try:
             net_stats = collect_stats_for_netdev(netdev)
-            msg_stats = collect_message_stats_from_splunk(host)
+            msg_stats = collect_message_stats_from_splunk(my_hostname=host,
+                                                          splunk_server=splunk_search_hostname,
+                                                          username='ris-api',
+                                                          password='TRGZnUaJFUcrFY5sXzwSFrPn')
+
+            record_stats(net_stats, msg_stats)
 
         except Exception as e:
             sys.stderr.write('Caught exception during main loop:\n')
